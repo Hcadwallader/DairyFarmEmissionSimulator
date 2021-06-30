4b5302ef0b652f03dafd8b81a18492230bea60a1
Helen Schwan <helenschwan3@gmail.com>
2021-06-30 12:47:45 +0100

    final edits

--- Frontend/src/components/EmissionsReport.js
+++ Frontend/src/components/EmissionsReport.js
@@ -5,10 +5,13 @@ const EmissionReport = ({ emissionData }) => (
 			<p>Diesel: {emissionData.scope1} kgCO2e </p>
 			<p>Electricity: {emissionData.scope2} kgCO2e</p>
 			<p>Food: {emissionData.scope3} kgCO2e</p>
-			<p>Total emissions: {emissionData.totalEmissions} kgCO2e</p>
+			<p>
+				Total emissions:{' '}
+				{parseInt(emissionData.totalEmissions).toFixed(2)} kgCO2e
+			</p>
 			<p>
 				Emissions per litre of milk: {emissionData.perLitreOfMilk}{' '}
-				kgCO2e
+				kgCO2e/litre
 			</p>
 		</div>
 	</div>
